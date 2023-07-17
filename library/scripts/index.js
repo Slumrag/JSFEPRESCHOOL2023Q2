673990c66cc8d87563549a2c7e5b8a82c027f1c2
Slumrag <mrgreenthecristal@yandex.ru>
2023-07-17 20:13:56 +0300

    feat: add evaluation

--- library/scripts/index.js
+++ library/scripts/index.js
@@ -1,3 +1,10 @@
-const EVALUATION_MESSAGE = `Оценка работы:
+const EVALUATION_MESSAGE = `Оценка работы: 100
+1. Вёрстка валидная +10
+2. Вёрстка семантическая +16
+3. Вёрстка соответствует макету +54
+4. Общие требования к верстке +20
 `;
+const DEPLOY_LINK =
+	"https://rolling-scopes-school.github.io/slumrag-JSFEPRESCHOOL2023Q2/library/";
 console.info(EVALUATION_MESSAGE);
+console.log(DEPLOY_LINK);
