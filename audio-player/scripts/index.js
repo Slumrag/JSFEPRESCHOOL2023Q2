eb1a4160b3123dda6a721098df0e5b8df2c89f0b
Slumrag <mrgreenthecristal@yandex.ru>
2023-09-23 22:55:51 +0300

    feat: add mute button and volume slider

--- audio-player/scripts/index.js
+++ audio-player/scripts/index.js
@@ -27,3 +27,30 @@ document.addEventListener('input', (event) => {
 	console.log('change input');
 	jumpTimeline();
 });
+//mute
+document.addEventListener('click', (event) => {
+	if (!event.target.closest('#mute')) return;
+	document.querySelector('#mute').classList.toggle('volume__mute_enable');
+	if (audio.muted) {
+		audio.muted = false;
+	} else {
+		audio.muted = true;
+	}
+});
+//show volume slider
+document.addEventListener('mouseover', (event) => {
+	if (!event.target.closest('#mute') && !event.target.closest('.volume__range-wrapper')) return;
+	document.querySelector('.volume__range-wrapper').classList.add('volume__range_visible');
+});
+//hide volume slider
+document.addEventListener('mouseout', (event) => {
+	if (!event.target.closest('.volume__range-wrapper')) return;
+	document.querySelector('.volume__range-wrapper').classList.remove('volume__range_visible');
+});
+//change volume
+document.addEventListener('input', (event) => {
+	if (!event.target.closest('#song-volume')) return;
+	const volumeRange = document.querySelector('#song-volume');
+	audio.volume = +volumeRange.value / 100;
+});
+console.log('https://rolling-scopes-school.github.io/slumrag-JSFEPRESCHOOL2023Q2/audio-player');
